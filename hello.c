ffee960c8fe484911ae836351e1ecf349cc98451
Amar Singh <amar298976@gmail.com>
2024-11-27 16:02:56 +0530

    Create hello.c
    add c file code

---
+++ hello.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+AREA OF CIRCLE CODE 
+int main(){
+    float radius;
+    printf("enter the radius");
+    scanf("%f", &radius);
+
+    // printf("area of circle will be %f", 3.14*radius*radius);
+    int b = 3.14*radius *radius;
+    printf("area will be %f", b);
+    return 0 ;
+}
+AREA OF SQUARE CODE 
+    int main(){
+    int side;
+    printf("enter side");
+    scanf("%d", &side);
+    
+    printf("area will be %d", side*side);
+    // int a = side*side;
+    // printf("side is %d",a);
+    return 0;
+}
