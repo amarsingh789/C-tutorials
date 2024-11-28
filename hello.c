1e17ccceac8ea0f3757f9f97b256d05a35e2e45a
Amar Singh <amar298976@gmail.com>
2024-11-28 16:26:45 +0530

    Update hello.c
    adding grade option

--- hello.c
+++ hello.c
@@ -85,3 +85,20 @@ int marks;
         printf("wrong number\n");
     }
     return 0 ;
+int marks;
+    printf("enter the number(1-100):");
+    scanf("%d", &marks);
+    if (marks < 30){
+       printf("C\n");
+    }
+    else if (marks >= 30 && marks < 70){
+        printf("B\n");
+    }
+    else if (marks >= 70 && marks < 90)
+    {
+       printf("A\n");
+    }
+    else{
+        printf("A+");
+    }
+    return 0;
