473f8181ac687f2a7f6a09b8fa1197dd1c5a6f8b
Amar Singh <amar298976@gmail.com>
2024-11-29 15:14:21 +0530

    Update chapter-4.c
    adding char,float

--- chapter-4.c
+++ chapter-4.c
@@ -4,5 +4,11 @@ int main(){
    for(int i=1; i<= 10; i=i+1 ){
         printf("Amar Singh\n");
     }
+    for(float i =1.0; i<= 5.0; i=i+1){
+        printf("%f\n",i);
+    }
+    for(char ch='a'; ch<='z'; ch++){
+        printf("%c\n",ch);
+    }
      return 0;
 }
