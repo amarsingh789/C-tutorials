d7ed65f4d8e67e7d9c3778afe70e22e2dd4e1f8d
Amar Singh <amar298976@gmail.com>
2024-11-29 15:31:54 +0530

    Update chapter-4.c
    enter while loop

--- chapter-4.c
+++ chapter-4.c
@@ -10,5 +10,15 @@ int main(){
     for(char ch='a'; ch<='z'; ch++){
         printf("%c\n",ch);
     }
+   //while loop
+    int n;
+    printf("enter the no: ");
+    scanf("%d",&n);
+
+    int i = 0;
+    while(i<= n){
+        printf("%d\n", i);
+        i++;
+    }
      return 0;
 }
