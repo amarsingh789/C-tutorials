e61c625a68014f6bcb092b4b1db2dd69ed4e121d
Amar Singh <amar298976@gmail.com>
2024-11-30 13:13:58 +0530

    Update chapter-4.c
    adding question -8,9

--- chapter-4.c
+++ chapter-4.c
@@ -102,5 +102,22 @@ for(int i =1; i<=n; i++){
   fact = fact*i;
 }
 printf("final factorial is %d", fact);
+
+  //question-08
+int n;
+printf("enter the number: ");
+scanf("%d",&n);
+for (int i = 10; i >=1; i--)
+{
+ printf("%d\n", n*i);
+}
+
+//QUESTION-09
+int sum = 0;
+for (int i = 5; i <=50; i++){
+   sum+= i;
+}
+printf("sum is %d", sum);
+ 
      return 0;
 }
