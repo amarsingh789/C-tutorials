86f97946c82bbb3f6b6ae15bd2da0e8f70205283
Amar Singh <amar298976@gmail.com>
2024-11-30 16:36:56 +0530

    Update chapter-05.c
    adding question

--- chapter-05.c
+++ chapter-05.c
@@ -19,3 +19,27 @@ void printHello(){
 void printGoodbye(){
     printf("Goodbye\n");
 }
+
+//question-01
+void nameste();
+void bonjour();
+
+int main(){
+    printf("enter the character india i & french f  : ");
+    char ch;
+    scanf("%c", &ch);
+    if (ch == 'i')
+    {
+        nameste();
+    }else{
+        bonjour();
+    }
+    return 0;
+}
+
+void nameste(){
+    printf("Nameste\n");
+}
+void bonjour(){
+    printf("Bonjour\n");
+}
