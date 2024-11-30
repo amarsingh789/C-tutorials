02dfbcf2c250df00cc6946d2d19384691879ee9e
Amar Singh <amar298976@gmail.com>
2024-11-30 16:25:30 +0530

    Update chapter-05.c
    adding function code

--- chapter-05.c
+++ chapter-05.c
@@ -1,1 +1,21 @@
 //this is function and recursion file 
+//START WITH FUNTION
+#include<stdio.h>
+//declaration/prototype
+void printHello();
+void printGoodbye();
+
+int main(){
+    printHello();
+    printGoodbye();
+    return 0;
+}
+
+//function 
+void printHello(){
+    printf("hello World!\n");
+    // printf("my name is amar\n");
+}
+void printGoodbye(){
+    printf("Goodbye\n");
+}
