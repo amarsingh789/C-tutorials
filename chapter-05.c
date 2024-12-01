2af63ad7e86fedbbd9fd1a54f157e9fc5807d9ce
Amar Singh <amar298976@gmail.com>
2024-12-01 23:39:41 +0530

    Update chapter-05.c
    add question 02

--- chapter-05.c
+++ chapter-05.c
@@ -43,3 +43,21 @@ void nameste(){
 void bonjour(){
     printf("Bonjour\n");
 }
+
+//question-02
+
+int sum(int a, int b);
+int main(){
+    int a,b;
+    printf("enter first number: ");
+    scanf("%d",&a);
+    printf("enter second number: ");
+    scanf("%d", &b);
+
+    int s = sum(a,b);
+    printf("sum is %d  :" , s );
+    return 0;
+}
+int sum(int x , int y){
+    return x + y;
+}
