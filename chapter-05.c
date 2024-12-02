fef2a51ba75187734773e0103735e43a24ecd873
Amar Singh <amar298976@gmail.com>
2024-12-02 12:07:29 +0530

    Update chapter-05.c
    adding recursion

--- chapter-05.c
+++ chapter-05.c
@@ -104,3 +104,19 @@ float circleArea(float radius){
 float rectangleArea(float a, float b){
     return a*b;
 }
+
+//RECURSION FUNCTION
+int fact(int n);
+
+int main(){
+     printf("factorial will be %d", fact(5));
+}
+
+int fact(int n){
+    if(n == 0){
+       return 1;
+    }
+    int factNum1 =fact(n-1);
+    int factN = factNum1 * n;
+    return factN;  
+}
