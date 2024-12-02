da9e1992ac6c837a75e0f25856e05c1c401c254f
Amar Singh <amar298976@gmail.com>
2024-12-02 10:51:04 +0530

    Update chapter-05.c
    adding table question

--- chapter-05.c
+++ chapter-05.c
@@ -61,3 +61,21 @@ int main(){
 int sum(int x , int y){
     return x + y;
 }
+//print table using function
+void printTable(int n);
+
+int main(){
+    int n;
+    printf("enter the number: ");
+    scanf("%d", &n);
+
+    printTable(n); //argument
+
+    return 0;
+}
+
+void printTable(int n){//parameter
+    for(int i =1; i<=10;i++){
+        printf("%d\n", i*n);
+    }
+}
