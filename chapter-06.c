685e786e23eee2016ce2cda9c1d7475c2abee0fb
Amar Singh <amar298976@gmail.com>
2024-12-03 11:17:22 +0530

    Update chapter-06.c
    giving address of machine

--- chapter-06.c
+++ chapter-06.c
@@ -1,1 +1,12 @@
 //this file is pointer file
+//this code give address of machine
+int main(){
+    int age = 3;
+    int *ptr = &age;
+    //address
+    //printf("%p\n", &age);
+    printf("%u\n", &age);
+
+    printf("%u\n", ptr);
+     printf("%u\n", &ptr);
+    return 0;
