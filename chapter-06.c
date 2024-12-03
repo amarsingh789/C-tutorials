a7cf1d9dbb0c6489d3fd01330d02618874815e20
Amar Singh <amar298976@gmail.com>
2024-12-03 12:16:07 +0530

    Update chapter-06.c
    adding question-02

--- chapter-06.c
+++ chapter-06.c
@@ -31,3 +31,16 @@ int main(){
 
     return 0;
 }
+//    QUESTION-02
+void printAddress(int n);
+
+int main(){
+    int n = 4;
+    printAddress(n);
+    printf("address of n is %u\n",&n );
+    return 0;
+}
+
+void printAddress(int n){
+    printf("address of n is %u\n",&n );
+}
