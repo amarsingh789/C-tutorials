16fe5a667f75d5631ef74ed09274f4701de74316
Amar Singh <amar298976@gmail.com>
2024-12-03 21:48:02 +0530

    Update chapter-07.c
    adding int, float and char concept

--- chapter-07.c
+++ chapter-07.c
@@ -15,3 +15,50 @@ int main(){
     printf("physics = %d, math = %d, chemistry = %d", marks[0],marks[1],marks[2] );
 
 }
+//QUESTION-01
+int main(){
+    float price[3];
+    printf("enter the 3 price: ");
+    scanf("%f", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+
+    printf("total price-1: %f\n",price[0]+(0.18*price[0])); 
+    printf("total price-2: %f\n",price[1]+(0.18*price[1])); 
+    printf("total price-3: %f\n",price[2]+(0.18*price[2])); 
+    return 0;
+}
+//CONCEPT OF INT & FLOAT=4-BIT AND CHAR = 1-BIT
+//INT CONCEPT
+int main(){
+    int age = 22;
+    int *ptr = &age;
+    printf("ptr = %u\n",ptr);
+    ptr++;
+    printf("ptr = %u\n",ptr);
+    ptr--;
+    printf("ptr = %u",ptr);
+    return 0;
+}
+//FLOAT CONCEPT
+int main(){
+    float price = 100.0;
+    float *ptr = &price;
+    printf("ptr = %u\n",ptr);
+    ptr++;
+    printf("ptr = %u\n",ptr);
+    ptr--;
+    printf("ptr = %u",ptr);
+    return 0;
+}
+//CHAR CONCEPT
+int main(){
+    char star = '*';
+    char *ptr = &star;
+    printf("ptr = %u\n",ptr);
+    ptr++;
+    printf("ptr = %u\n",ptr);
+    ptr--;
+    printf("ptr = %u",ptr);
+    return 0;
+}
