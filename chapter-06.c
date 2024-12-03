0d5e423fc3f07cdfdf27d37de8827e4c1ef787b5
Amar Singh <amar298976@gmail.com>
2024-12-03 11:26:27 +0530

    Update chapter-06.c
    adding question

--- chapter-06.c
+++ chapter-06.c
@@ -10,3 +10,24 @@ int main(){
     printf("%u\n", ptr);
      printf("%u\n", &ptr);
     return 0;
+//QUESTION-01
+int main(){
+    int x;
+    int *ptr;
+
+    ptr = &x;
+    *ptr = 0;
+
+    printf("x =%d\n", x);
+    printf("*ptr = %d\n", *ptr);
+
+    *ptr += 5;
+    printf("x =%d\n", x);
+    printf("*ptr = %d\n", *ptr);
+
+    (*ptr)++;
+    printf("x =%d\n", x);
+    printf("*ptr = %d\n", *ptr);
+
+    return 0;
+}
