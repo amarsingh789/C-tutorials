f55618f6d9d05f308bb969b3beb750595b3c7462
Amar Singh <amar298976@gmail.com>
2024-12-03 16:11:28 +0530

    Update chapter-07.c
    adding basic array question

--- chapter-07.c
+++ chapter-07.c
@@ -1,1 +1,17 @@
 //this file all abouts for arrays 
+#include<stdio.h>
+
+int main(){
+    int marks[3];
+    printf("enter physics marks: ");
+    scanf("%d", &marks[0]);
+
+    printf("enter math marks :  ");
+    scanf("%d", &marks[1]);
+
+    printf("enter chemistry marks: ");
+    scanf("%d", &marks[3]);
+
+    printf("physics = %d, math = %d, chemistry = %d", marks[0],marks[1],marks[2] );
+
+}
