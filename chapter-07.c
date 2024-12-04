d63e4ee423c4dc6b357fd6e3fddba314bea01014
Amar Singh <amar298976@gmail.com>
2024-12-04 11:00:03 +0530

    Update chapter-07.c
    adding question

--- chapter-07.c
+++ chapter-07.c
@@ -62,3 +62,20 @@ int main(){
     printf("ptr = %u",ptr);
     return 0;
 }
+
+int main(){
+
+    int aadhaar[5];
+    //input
+    int *ptr = &aadhaar[0];
+    for(int i =0; i<5; i++){
+        printf("%d index: ", i);
+        scanf("%d",(ptr+i));
+    }
+
+    //output
+    for(int i = 0; i<5; i++){
+        printf("%d index = %d\n",i,*(ptr+i));
+    }
+    return 0;
+}
