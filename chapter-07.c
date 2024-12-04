f2ff8875efc9456661f902568d2a0b8be5a39d5b
Amar Singh <amar298976@gmail.com>
2024-12-04 11:44:00 +0530

    Update chapter-07.c
    add question-02

--- chapter-07.c
+++ chapter-07.c
@@ -109,3 +109,21 @@ int main(){
     printf("%d",marks[1][2]);
     return 0;
 }
+//QUESTION-02
+int countEven(int arr[],int n);
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,8};
+    printf("%d", countEven(arr , 7));
+    return 0;
+}
+
+int countEven(int arr[],int n){
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i]% 2 == 0){//even
+        count++;
+        }
+    }
+    return count;
+}
