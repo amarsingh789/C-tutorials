4cd12b6cc20bd01b6bbbd257b09e1ff7d146f27c
Amar Singh <amar298976@gmail.com>
2024-12-05 11:00:39 +0530

    Update chapter-08.c
    add how to write string code

--- chapter-08.c
+++ chapter-08.c
@@ -1,1 +1,20 @@
 //this file is string concept and code
+#include<stdio.h>
+
+void printString(char arr[]);
+
+int main(){
+    char firstName[] = "AMAR";
+    char lastName[] = "SINGH";
+
+    printString(firstName);
+    printString(lastName);
+    return 0;
+}
+
+void printString(char arr[]){
+    for(int i = 0; arr[i] != '\0';i++){
+        printf("%c",arr[i]);
+    }
+    printf("\n");
+}
