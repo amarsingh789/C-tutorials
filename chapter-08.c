1c0b84e56da95b7c3f7b9796bf7905e49779c4e2
Amar Singh <amar298976@gmail.com>
2024-12-05 11:57:51 +0530

    Update chapter-08.c
    find length of first name

--- chapter-08.c
+++ chapter-08.c
@@ -18,3 +18,20 @@ void printString(char arr[]){
     }
     printf("\n");
 }
+//to find length of first name
+int countLength(char arr[]);
+
+int main(){
+    char name[100];
+    fgets(name, 100, stdin);
+    printf("length is : %d",countLength(name));
+    return 0;
+}
+
+int countLength(char arr[]){
+    int count = 0;
+    for(int i=0;arr[i]!='\0';i++){
+        count++;
+    }
+    return count-1;
+}
